d8405b6fb281ea201725b579106c88f7a5b9ca0b
Igor Luiz Batista da Silva <1241301295@acd.grupotiradentes.com>
2024-10-28 19:24:45 -0300

    first commit

---
+++ src/main/java/com/igor/apineuro/entity/Employee.java
@@ -0,0 +1,73 @@
+package com.igor.apineuro.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	private String name; 
+	private String phone; 
+	private String email; 
+	private String experienceTime; 
+	private String linkedinUrl;
+	
+	public Employee() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getExperienceTime() {
+		return experienceTime;
+	}
+
+	public void setExperienceTime(String experienceTime) {
+		this.experienceTime = experienceTime;
+	}
+
+	public String getLinkedinUrl() {
+		return linkedinUrl;
+	}
+
+	public void setLinkedinUrl(String linkedinUrl) {
+		this.linkedinUrl = linkedinUrl;
+	} 
+	
+	
+}
