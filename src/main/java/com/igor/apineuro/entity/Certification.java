4d91f460a88a7d88f2d9da43434d6ce0d88a400c
Igor Luiz Batista da Silva <1241301295@acd.grupotiradentes.com>
2024-10-30 17:56:41 -0300

    criando os atributos

--- src/main/java/com/igor/apineuro/entity/Certification.java
+++ src/main/java/com/igor/apineuro/entity/Certification.java
@@ -1,6 +1,33 @@
 package com.igor.apineuro.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
 public class Certification {
+	@Id
+	@GeneratedValue
+	
+	private Long id; 
+	private String name; 
+	private String description; 
+	private String institution; 
+	private LocalDate starDate; 
+	private LocalDate expirationDate; 
+	
+	@ManyToOne (
+			fetch = FetchType.LAZY,
+			cascade = CascadeType.ALL 
+)
+	@JoinColumn (name = "employee_id")
+	public Employee employee;
+	
+		
 	
 
 }
